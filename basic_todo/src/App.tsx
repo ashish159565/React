827cc4d621e1c7ec005f27339ceab5138119b032
ASHISH GAJJELA <ashishgajjala@rediffmail.com>
2023-04-15 13:41:23 +0530

    fixing warrning

--- basic_todo/src/App.tsx
+++ basic_todo/src/App.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './App.css';
 import InputField from './components/InputField';
 import TodoList from './components/TodoList';
-import { useSearchParams } from 'react-router-dom';
 import { Todo } from "./model";
 
 const App = () => {
--- basic_todo/src/components/InputField.tsx
+++ basic_todo/src/components/InputField.tsx
@@ -1,4 +1,4 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { useRef } from 'react';
 import './styles.css';
 
 interface Props{
